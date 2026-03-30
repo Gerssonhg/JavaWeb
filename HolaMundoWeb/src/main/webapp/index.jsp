a80dbb61b1264d64128ae4d1fe85034c68f4f365
Gerssonhg <gerssonhg.19@gmail.com>
2026-03-30 17:12:29 -0500

    Primer commint video 6

---
+++ HolaMundoWeb/src/main/webapp/index.jsp
@@ -0,0 +1,19 @@
+<%-- 
+    Document   : index
+    Created on : 17 mar. 2026, 09:50:25
+    Author     : Gersson
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hola mundo desde HTML!</h1>
+        <% String hola = "Hola mundo desde JAVA";%>
+        <%= hola%>
+    </body>
+</html>
---
+++ PrimerServlet/src/main/java/com/cursojava/primerservlet/SvUsuarios.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package com.cursojava.primerservlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Gersson
+ */
+@WebServlet(name = "SvUsuarios", urlPatterns = {"/SvUsuarios"})
+public class SvUsuarios extends HttpServlet {
+
+  
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet SvUsuarios</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet SvUsuarios at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+  
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+        
+        
+    }
+
+   
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        String dni = request.getParameter("dni");
+        String nombre = request.getParameter("nombre");
+        String apellido = request.getParameter("apellido");
+        String telefono = request.getParameter("telefono");
+        
+        System.out.println("DNI es: "+ dni);
+        System.out.println("Nombre es: "+ nombre);
+        System.out.println("Apellido es: "+ apellido);
+        System.out.println("Telefono es: "+ telefono);
+    }
+
+  
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
